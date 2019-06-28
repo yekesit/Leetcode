8e32cf72b668c76a38805ca476c9cefd4efcb752
Ke Ye <yekesit@gmail.com>
2019-06-27 20:58:52 -0700

    LT-957 simple cycle

---
+++ LT-957 Prison Cells After N Days.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Ke Ye on 2019-06-27.
+//
+
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+
+
+void nextDay(vector<int>& cells, int& day){
+    vector<int> next(8, 0);
+    for(int i = 1; i < 7; i++){
+        next[i] = cells[i - 1] == cells[i + 1];
+    }
+    cells = next;
+    cout << setw(2) << day << ":  ";
+    for(auto c : cells){
+        cout << c << ' ';
+    }
+    cout << endl;
+    day++;
+}
+
+int main() {
+    vector<int> cells{0, 1, 0, 1, 1, 0, 0, 1};
+    int N = 30;
+    int day = 0;
+    while(N--){
+        nextDay(cells, day);
+    }
+}
