515fb6421f860a36bf51ff2b984c0dd4b1c04ae4
Ke Ye <yekesit@gmail.com>
2019-06-29 13:14:56 -0700

    LT-957 Just write it as class

--- LT-957 Prison Cells After N Days.cpp
+++ LT-957 Prison Cells After N Days.cpp
@@ -8,27 +8,31 @@
 
 using namespace std;
 
+class Solution{
+public:
+    void nextDay(vector<int>& cells, int& day){
+        vector<int> next(8, 0);
+        for(int i = 1; i < 7; i++){
+            next[i] = cells[i - 1] == cells[i + 1];
+        }
+        cells = next;
+        cout << setw(2) << day << ":  ";
+        for(auto c : cells){
+            cout << c << ' ';
+        }
+        cout << endl;
+        day++;
+    }
+};
 
 
-void nextDay(vector<int>& cells, int& day){
-    vector<int> next(8, 0);
-    for(int i = 1; i < 7; i++){
-        next[i] = cells[i - 1] == cells[i + 1];
-    }
-    cells = next;
-    cout << setw(2) << day << ":  ";
-    for(auto c : cells){
-        cout << c << ' ';
-    }
-    cout << endl;
-    day++;
-}
 
 int main() {
     vector<int> cells{0, 1, 0, 1, 1, 0, 0, 1};
     int N = 30;
     int day = 0;
+    Solution s;
     while(N--){
-        nextDay(cells, day);
+        s.nextDay(cells, day);
     }
 }
