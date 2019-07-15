eafba2f0d14f52e01e9e99b37a9d15c4663c8912
Ke Ye <yekesit@gmail.com>
2019-07-15 11:20:21 -0700

    an OA

---
+++ find_k_char_in_string_pattern.cpp
@@ -0,0 +1,46 @@
+//
+// Created by Ke Ye on 2019-07-15.
+//
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+
+
+
+char findK(string& str, int k){
+    int sum = 0;
+    int left = 0;
+    int right = 0;
+    int len = str.length();
+
+    while(left < len && right < len){
+        while(right < len && isalpha(str[right])) right++;
+        string cur = str.substr(left, right - left);
+        left = right;
+        while(right < len && isdigit(str[right])) right++;
+        int count = stoi(str.substr(left, right - left));
+        if(cur.length() * count + sum - 1 >= k){
+            int index = (k - sum) % cur.length();
+            return cur[index];
+        }
+        else{
+            sum += cur.length() * count;
+        }
+        left = right;
+    }
+    return '0';
+}
+
+
+
+
+
+int main(){
+    string str = "spring5frog10";
+    int k = 29;
+    cout << findK(str, k) << endl;
+}
